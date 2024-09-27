1597499d85b3cdbf09b19f6591f81b9624cf7ff2
Edwin <lgedwin.martinez@gmail.com>
2024-09-26 20:38:53 -0500

    llenado y recorrido de un arreglo

--- scrip.js
+++ scrip.js
@@ -18,12 +18,26 @@
 
 // RECORRIDO EN LOS ARREGLOS 
 
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//.length determina cuantos elemetos hay en el arreglo y termina en la ultima posocion 
-for (let index = 0; index < numeros.length; index++) {
-    if (numeros[index] % 2 == 0) {
-        console.log(numeros[index])
+// let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// //.length determina cuantos elemetos hay en el arreglo y termina en la ultima posocion 
+// for (let index = 0; index < numeros.length; index++) {
+//     if (numeros[index] % 2 == 0) {
+//         console.log(numeros[index])
 
-    }
+//     }
 
+
+
+
+// LLENADO EN LOS ARREGLOS 
+
+let nombres = []
+for (let index = 0; index < 5; index++) {
+    nombres[index] = prompt('ingrese su nombre:')
+    
 }
+ //RECORRIDO LUEGO DE LLENAR EL ARREGLO
+ for (let index = 0; index < nombres.length; index++) {
+   console.log(nombres[index])
+    
+ }
